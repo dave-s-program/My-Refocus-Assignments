9241f6da7871c5fb404933e62c5f259426ad0682
Dave Flores Malhin <suppordev199x@gmail.com>
2022-11-24 18:44:19 +0800

    First New Assignment: BMI Calculator

---
+++ bmi_calculator.js
@@ -0,0 +1,21 @@
+
+function calculateBMI(kg, meter){
+    return kg / (meter * meter);
+}
+
+let kg = 65;
+let meter = 1.7
+
+const bmi = calculateBMI(kg, meter);
+
+if (bmi <= 18.5) {
+    output = "Under Weight";
+} else if(bmi > 18.5 && bmi <= 24.9){
+    output = "Normal Weight";
+} else if(bmi > 24.9 && bmi <= 29.9){
+    output = "Over Weight";
+} else if(bmi > 30.0){
+    output = "OBESE";
+}
+
+console.log("Your BMI value is " +bmi.toFixed(0)+ " and you are : "+output);
